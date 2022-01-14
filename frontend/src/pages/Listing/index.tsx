76b523eae2bddcd73c682a53f377469d7ae6682a
lucasmaciiel <lucasmacielgois@gmail.com>
2022-01-14 14:42:07 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,15 +2,25 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/paginations";
+import { useEffect } from "react";
+import { useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=20&page=0`)
-    .then(response => {
-        console.log(response.data);
-    })
+    const [pageNumber, setPageNumber] = useState(0);
+    
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=20&page=1`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+    
+        })
+    }, []);
+
 
     return (
         <>
